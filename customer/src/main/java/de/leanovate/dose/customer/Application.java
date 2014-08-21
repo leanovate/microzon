2669aee1e4c4a782e4ba7a556705a19772c9b7b6
Untoldwind <landru@untoldwind.net>
2014-08-21 23:16:41 +0200

    Customer service traces data to zipkin

--- customer/src/main/java/de/leanovate/dose/customer/Application.java
+++ customer/src/main/java/de/leanovate/dose/customer/Application.java
@@ -1,6 +1,13 @@
 package de.leanovate.dose.customer;
 
+import com.github.kristofa.brave.Brave;
+import com.github.kristofa.brave.EndPointSubmitter;
+import com.github.kristofa.brave.ServerTracer;
+import com.github.kristofa.brave.SpanCollector;
+import com.github.kristofa.brave.TraceFilter;
+import com.github.kristofa.brave.zipkin.ZipkinSpanCollector;
 import de.leanovate.dose.customer.filter.LoggingFilter;
+import de.leanovate.dose.customer.filter.ServletTraceFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.embedded.FilterRegistrationBean;
@@ -8,7 +15,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
 @ComponentScan
@@ -17,6 +27,7 @@ public class Application {
 
     @Bean
     public FilterRegistrationBean loggingFilter() {
+
         final FilterRegistrationBean filterRegistration = new FilterRegistrationBean();
 
         filterRegistration.setFilter(new LoggingFilter());
@@ -24,7 +35,38 @@ public class Application {
         return filterRegistration;
     }
 
+    @Bean
+    public FilterRegistrationBean traceFilter(ServerTracer serverTracer, EndPointSubmitter endPointSubmitter)
+            throws UnknownHostException {
+
+        endPointSubmitter.submit(InetAddress.getLocalHost().getHostAddress(), 8080, "customer");
+        final FilterRegistrationBean filterRegistration = new FilterRegistrationBean();
+
+        filterRegistration.setFilter(new ServletTraceFilter(serverTracer));
+        filterRegistration.setUrlPatterns(Arrays.asList("/*"));
+        return filterRegistration;
+    }
+
+    @Bean
+    public ServerTracer serverTracer(SpanCollector spanCollector) {
+
+        return Brave.getServerTracer(spanCollector, Collections.<TraceFilter>emptyList());
+    }
+
+    @Bean
+    public SpanCollector spanCollector() {
+
+        return new ZipkinSpanCollector("192.168.254.20", 9410);
+    }
+
+    @Bean
+    public EndPointSubmitter endPointSubmitter() {
+
+        return Brave.getEndPointSubmitter();
+    }
+
     public static void main(String[] args) throws Exception {
+
         SpringApplication.run(Application.class, args);
     }
 }
