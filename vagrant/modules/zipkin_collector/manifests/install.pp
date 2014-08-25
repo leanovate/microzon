72a90226b1f9c078f54fa72e1124c6bfc823d0e9
Untoldwind <landru@untoldwind.net>
2014-08-25 23:58:07 +0200

    Use special vendor branch of zipkin

--- vagrant/modules/zipkin_collector/manifests/install.pp
+++ vagrant/modules/zipkin_collector/manifests/install.pp
@@ -11,55 +11,58 @@ class zipkin_collector::install {
         ensure => present
     }
 
-    file { "/opt/app":
+    file { ["/opt/app", "/opt/app/zipkin-collector-service", "/opt/app/zipkin-query-service", "/opt/app/zipkin-web"]:
         ensure => directory,
         owner => root,
         group => root,
         mode => 755
     }
 
-    exec { "download collector":
-        command => "/usr/bin/wget https://github.com/twitter/zipkin/releases/download/1.1.0/zipkin-collector-service.zip",
-        cwd => "/tmp",
-        creates => "/tmp/zipkin-collector-service.zip"
+    file { "/opt/app/zipkin-collector-service.zip":
+        owner => root,
+        group => root,
+        mode => 644,
+        ensure => file,
+        source => "file:///vagrant/vendor/zipkin-collector-service.zip",
     }
 
     exec { "unpack collector":
-        command => "/bin/rm -rf zipkin-collector-service; /usr/bin/unzip /tmp/zipkin-collector-service.zip; /bin/mv zipkin-collector-service-1.1.0 zipkin-collector-service",
-        cwd => "/opt/app",
-        subscribe => Exec["download collector"],
-        require => Package["unzip"]
+        command => "/usr/bin/unzip /opt/app/zipkin-collector-service.zip",
+        cwd => "/opt/app/zipkin-collector-service",
+        subscribe => File["/opt/app/zipkin-collector-service.zip"],
+        require => Package["unzip"],
+        refreshonly => true
     }
 
-    exec { "download query":
-        command => "/usr/bin/wget https://github.com/twitter/zipkin/releases/download/1.1.0/zipkin-query-service.zip",
-        cwd => "/tmp",
-        creates => "/tmp/zipkin-query-service.zip"
+    file { "/opt/app/zipkin-query-service.zip":
+        owner => root,
+        group => root,
+        mode => 644,
+        ensure => file,
+        source => "file:///vagrant/vendor/zipkin-query-service.zip",
     }
 
     exec { "unpack query":
-        command => "/bin/rm -rf zipkin-query-service; /usr/bin/unzip /tmp/zipkin-query-service.zip; /bin/mv zipkin-query-service-1.1.0 zipkin-query-service",
-        cwd => "/opt/app",
-        subscribe => Exec["download query"],
-        require => Package["unzip"]
+        command => "/usr/bin/unzip /opt/app/zipkin-query-service.zip",
+        cwd => "/opt/app/zipkin-query-service",
+        subscribe => File["/opt/app/zipkin-query-service.zip"],
+        require => Package["unzip"],
+        refreshonly => true
     }
 
-    exec { "download web":
-        command => "/usr/bin/wget https://github.com/twitter/zipkin/releases/download/1.1.0/zipkin-web.zip",
-        cwd => "/tmp",
-        creates => "/tmp/zipkin-web.zip"
+    file { "/opt/app/zipkin-web.zip":
+        owner => root,
+        group => root,
+        mode => 644,
+        ensure => file,
+        source => "file:///vagrant/vendor/zipkin-web.zip",
     }
 
     exec { "unpack web":
-        command => "/bin/rm -rf zipkin-web; /usr/bin/unzip /tmp/zipkin-web.zip; /bin/mv zipkin-web-1.1.0 zipkin-web",
-        cwd => "/opt/app",
-        subscribe => Exec["download web"],
-        require => Package["unzip"]
-    }
-
-    exec { "unpack web resources":
-        command => "/bin/rm -rf resources; /bin/mkdir resources; cd resources; /usr/bin/jar xf ../zipkin-web-1.1.0.jar",
+        command => "/usr/bin/unzip /opt/app/zipkin-web.zip",
         cwd => "/opt/app/zipkin-web",
-        subscribe => Exec["unpack web"]
+        subscribe => File["/opt/app/zipkin-web.zip"],
+        require => Package["unzip"],
+        refreshonly => true
     }
 }
