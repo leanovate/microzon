c297e863620dfc6df972cb246196cfd59a703426
Untoldwind <landru@untoldwind.net>
2014-08-26 08:55:50 +0200

    Use own zipkin snapshot

--- vagrant/modules/zipkin_collector/manifests/install.pp
+++ vagrant/modules/zipkin_collector/manifests/install.pp
@@ -18,50 +18,44 @@ class zipkin_collector::install {
         mode => 755
     }
 
-    file { "/opt/app/zipkin-collector-service.zip":
-        owner => root,
-        group => root,
-        mode => 644,
-        ensure => file,
-        source => "file:///vagrant/vendor/zipkin-collector-service.zip",
+    exec { "download collector":
+        command => "/usr/bin/wget https://dl.dropboxusercontent.com/u/3815280/zipkin-collector-service.zip",
+        cwd => "/opt/app",
+        creates => "/opt/app/zipkin-collector-service.zip"
     }
 
     exec { "unpack collector":
         command => "/usr/bin/unzip /opt/app/zipkin-collector-service.zip",
         cwd => "/opt/app/zipkin-collector-service",
-        subscribe => File["/opt/app/zipkin-collector-service.zip"],
+        subscribe => Exec["download collector"],
         require => Package["unzip"],
         refreshonly => true
     }
 
-    file { "/opt/app/zipkin-query-service.zip":
-        owner => root,
-        group => root,
-        mode => 644,
-        ensure => file,
-        source => "file:///vagrant/vendor/zipkin-query-service.zip",
+    exec { "download query":
+        command => "/usr/bin/wget https://dl.dropboxusercontent.com/u/3815280/zipkin-query-service.zip",
+        cwd => "/opt/app",
+        creates => "/opt/app/zipkin-query-service.zip"
     }
 
     exec { "unpack query":
         command => "/usr/bin/unzip /opt/app/zipkin-query-service.zip",
         cwd => "/opt/app/zipkin-query-service",
-        subscribe => File["/opt/app/zipkin-query-service.zip"],
+        subscribe => Exec["download query"],
         require => Package["unzip"],
         refreshonly => true
     }
 
-    file { "/opt/app/zipkin-web.zip":
-        owner => root,
-        group => root,
-        mode => 644,
-        ensure => file,
-        source => "file:///vagrant/vendor/zipkin-web.zip",
+    exec { "download web":
+        command => "/usr/bin/wget https://dl.dropboxusercontent.com/u/3815280/zipkin-web.zip",
+        cwd => "/opt/app",
+        creates => "/opt/app/zipkin-web.zip"
     }
 
     exec { "unpack web":
         command => "/usr/bin/unzip /opt/app/zipkin-web.zip",
         cwd => "/opt/app/zipkin-web",
-        subscribe => File["/opt/app/zipkin-web.zip"],
+        subscribe => Exec["download web"],
         require => Package["unzip"],
         refreshonly => true
     }
