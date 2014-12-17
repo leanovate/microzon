18ee590e3d8746ee248c3e9c10114b6cabd913c3
Untoldwind <landru@untoldwind.net>
2014-12-17 14:55:55 +0100

    Working docker shop

--- docker/run.sh
+++ docker/run.sh
@@ -5,6 +5,10 @@ docker rm $(docker ps -a -q)
 
 docker run -d -p 8000:80 -P --name="log" "untoldwind/dose:log-v1"
 docker run -d -p 8001:80 -P --name="zipkin" "untoldwind/dose:zipkin-v1"
+
+# Uggly but necessary atm
+sleep 5
+
 docker run -d -p 3306:3306 -P --name="mysql" --link log:log-collector "untoldwind/dose:mysql-v2" 
 docker run -d -P --name="customer" --link mysql:mysql-db --link zipkin:zipkin-collector --link log:log-collector "untoldwind/dose:customer-v1" 
 docker run -d -p 8090:80 -P --name="product" --link zipkin:zipkin-collector --link log:log-collector "untoldwind/dose:product-v1" 
