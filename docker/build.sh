ed8cc37539763e50b2bf7284da265c0571766c43
Untoldwind <landru@untoldwind.net>
2014-12-19 16:54:07 +0100

    Working log

--- docker/build.sh
+++ docker/build.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-docker build -t "untoldwind/dose:base-v1" base
-docker build -t "untoldwind/dose:log-v1" log
+docker build -t "untoldwind/dose:base-v2" base
+docker build -t "untoldwind/dose:log-v2" log
 docker build -t "untoldwind/dose:zipkin-v1" zipkin
-docker build -t "untoldwind/dose:mysql-v2" mysql
-docker build -t "untoldwind/dose:customer-v1" customer
-docker build -t "untoldwind/dose:product-v1" product
-docker build -t "untoldwind/dose:cart-v1" cart
-docker build -t "untoldwind/dose:billing-v1" billing
-docker build -t "untoldwind/dose:web-v1" web
+docker build -t "untoldwind/dose:mysql-v3" mysql
+docker build -t "untoldwind/dose:customer-v2" customer
+docker build -t "untoldwind/dose:product-v2" product
+docker build -t "untoldwind/dose:cart-v2" cart
+docker build -t "untoldwind/dose:billing-v2" billing
+docker build -t "untoldwind/dose:web-v2" web
--- docker/run.sh
+++ docker/run.sh
@@ -3,15 +3,15 @@
 docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
 
-docker run -d -p 8000:80 -P --name="log" "untoldwind/dose:log-v1"
-docker run -d -p 8001:80 -P --name="zipkin" "untoldwind/dose:zipkin-v1"
+docker run -d -p 8000:80 -P --name="log" --hostname="log" "untoldwind/dose:log-v2"
+docker run -d -p 8001:80 -P --name="zipkin" --hostname="zipkin" "untoldwind/dose:zipkin-v1"
 
 # Uggly but necessary atm
 sleep 5
 
-docker run -d -p 3306:3306 -P --name="mysql" --link log:log-collector "untoldwind/dose:mysql-v2" 
-docker run -d -P --name="customer" --link mysql:mysql-db --link zipkin:zipkin-collector --link log:log-collector "untoldwind/dose:customer-v1" 
-docker run -d -p 8090:80 -P --name="product" --link zipkin:zipkin-collector --link log:log-collector "untoldwind/dose:product-v1" 
-docker run -d -P --name="cart" --link product:product-service --link customer:customer-service --link mysql:mysql-db --link zipkin:zipkin-collector --link log:log-collector "untoldwind/dose:cart-v1"
-docker run -d -P --name="billing" --link cart:cart-service --link product:product-service --link customer:customer-service --link mysql:mysql-db --link zipkin:zipkin-collector --link log:log-collector "untoldwind/dose:billing-v1"
-docker run -d -p 8080:80 -P --name="web" --link cart:cart-service --link product:product-service --link customer:customer-service --link billing:billing-service --link mysql:mysql-db --link zipkin:zipkin-collector --link log:log-collector "untoldwind/dose:web-v1"
+docker run -d -p 3306:3306 -P --name="mysql" --link log:log-collector --hostname="mysql" "untoldwind/dose:mysql-v3" 
+docker run -d -P --name="customer" --link mysql:mysql-db --link zipkin:zipkin-collector --link log:log-collector --hostname="customer" "untoldwind/dose:customer-v2" 
+docker run -d -p 8090:80 -P --name="product" --link zipkin:zipkin-collector --link log:log-collector --hostname="product" "untoldwind/dose:product-v2" 
+docker run -d -P --name="cart" --link product:product-service --link customer:customer-service --link mysql:mysql-db --link zipkin:zipkin-collector --link log:log-collector --hostname="cart" "untoldwind/dose:cart-v2"
+docker run -d -P --name="billing" --link cart:cart-service --link product:product-service --link customer:customer-service --link mysql:mysql-db --link zipkin:zipkin-collector --link log:log-collector --hostname="billing" "untoldwind/dose:billing-v2"
+docker run -d -p 8080:80 -P --name="web" --link cart:cart-service --link product:product-service --link customer:customer-service --link billing:billing-service --link mysql:mysql-db --link zipkin:zipkin-collector --link log:log-collector --hostname="web" "untoldwind/dose:web-v2"
