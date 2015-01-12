98e87ebf6ee69a95002142b664236b87f5d0f891
Untoldwind <landru@untoldwind.net>
2015-01-12 23:38:59 +0100

    Add some more alive checks

---
+++ docker/cart/sources/consul/alive.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import urllib2
+import sys
+
+try:
+	urllib2.urlopen("http://localhost:9990/health")
+except urllib2.HTTPError as e:
+	if e.code < 500:
+		sys.exit(1)
+	else:
+		sys.exit(255)
+except:
+	sys.exit(255)
+
+sys.exit(0)
