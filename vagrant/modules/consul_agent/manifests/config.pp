046eb47488f193c997a952e2475de162d67cd102
Untoldwind <landru@untoldwind.net>
2015-01-28 20:43:59 +0100

    Fix puppet

--- vagrant/modules/consul_agent/manifests/config.pp
+++ vagrant/modules/consul_agent/manifests/config.pp
@@ -1,13 +1,13 @@
 
 class consul_agent::config {
 	file { "/etc/supervisor/conf.d/consul.conf":
-        source => "puppet:///modules/consul/consul.conf",
+        source => "puppet:///modules/consul_agent/consul.conf",
         notify => Service["supervisor"],
         require => Package["supervisor"]
     }
 
 	file { "/etc/supervisor/conf.d/consul-template.conf":
-        source => "puppet:///modules/consul/consul-template.conf",
+        source => "puppet:///modules/consul_agent/consul-template.conf",
         notify => Service["supervisor"],
         require => Package["supervisor"]
     }
@@ -17,7 +17,7 @@ class consul_agent::config {
         group => root,
         mode => 644,
         ensure => file,
-        source => "puppet:///modules/consul/config.json"
+        source => "puppet:///modules/consul_agent/config.json"
     }
 
     file { "/etc/consul-templates/templates.conf":
@@ -25,7 +25,7 @@ class consul_agent::config {
         group => root,
         mode => 644,
         ensure => file,
-        source => "puppet:///modules/consul/templates.conf"
+        source => "puppet:///modules/consul_agent/templates.conf"
     }
 
     file { "/etc/consul-templates/logstash-forwarder.ctmpl":
@@ -33,6 +33,6 @@ class consul_agent::config {
         group => root,
         mode => 644,
         ensure => file,
-        source => "puppet:///modules/consul/logstash-forwarder.ctmpl"
+        source => "puppet:///modules/consul_agent/logstash-forwarder.ctmpl"
     }
 }
