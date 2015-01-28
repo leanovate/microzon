862ae2b1b96c0fd870cc6eafb10d5e012d9295da
Untoldwind <landru@untoldwind.net>
2015-01-28 11:51:21 +0100

    Fix puppet

--- vagrant/modules/consul/manifests/install.pp
+++ vagrant/modules/consul/manifests/install.pp
@@ -1,17 +1,17 @@
 class consul::install {
     exec { "download consul":
-        command => "/usr/bin/wget https://dl.bintray.com/mitchellh/consul/0.3.1_linux_amd64.zip",
+        command => "/usr/bin/wget https://dl.bintray.com/mitchellh/consul/0.4.1_linux_amd64.zip",
         cwd => "/tmp",
-        creates => "/tmp/0.3.1_linux_amd64.zip"
+        creates => "/tmp/0.4.1_linux_amd64.zip"
     }
 
     exec { "download consul ui":
-        command => "/usr/bin/wget https://dl.bintray.com/mitchellh/consul/0.3.1_web_ui.zip",
+        command => "/usr/bin/wget https://dl.bintray.com/mitchellh/consul/0.4.1_web_ui.zip",
         cwd => "/tmp",
-        creates => "/tmp/0.3.1_web_ui.zip"
+        creates => "/tmp/0.4.1_web_ui.zip"
     }
 
-    file { ["/var/lib/consul", "/usr/share/consul", "/etc/consul", "/etc/consul/conf.d"]:
+    file { ["/var/lib/consul", "/usr/local/share/consul", "/etc/consul", "/etc/consul/conf.d"]:
         ensure => directory,
         owner => root,
         group => root,
@@ -19,10 +19,18 @@ class consul::install {
     }
 
     exec { "unpack consul":
-        command => "/usr/bin/unzip /tmp/0.3.1_linux_amd64.zip",        
+        command => "/usr/bin/unzip /tmp/0.4.1_linux_amd64.zip",        
         cwd => "/usr/local/bin",
         creates => "/usr/local/bin/consul",
         subscribe => Exec["download consul"],
+        require => Package["unzip"],
+    }
+
+    exec { "unpack consul web ui":
+        command => "/usr/bin/unzip /tmp/0.4.1_web_ui.zip",        
+        cwd => "/usr/local/share/consul",
+        creates => "/usr/local/share/web",
+        subscribe => Exec["download consul ui"],
         require => Package["unzip"],
     }
 }
