f8c191b3b390c56e5b6df46a5be3a419f1353bf0
Untoldwind <landru@untoldwind.net>
2015-01-29 09:28:43 +0100

    Fix puppet

--- vagrant/manifests/site.pp
+++ vagrant/manifests/site.pp
@@ -21,6 +21,28 @@ node log {
 node mysql {
 	include "common"
     include "logstash_forwarder"
+    include "mysql"
 	include "consul_agent"
 	include "supervisor"
+
+    mysql_user { 'cart@localhost':
+        ensure        => present,
+        password_hash => mysql_password("cart"),
+        require => File["/root/.my.cnf"]
+    }
+
+    mysql_database { 'cart':
+        ensure => present,
+        require => File["/root/.my.cnf"]
+    }
+
+    mysql_grant { 'customer@localhost/cart.*':
+        ensure     => 'present',
+        options    => ['GRANT'],
+        privileges => ['ALL'],
+        table      => 'cart.*',
+        user       => 'cart@localhost',
+        require    => [Mysql_user['cart@localhost'], Mysql_database['cart']]
+    }    
+
 }
