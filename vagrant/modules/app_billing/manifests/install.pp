07fdb39539c62eed96051d1dfd256bc700907a96
Untoldwind <landru@untoldwind.net>
2015-02-03 09:50:09 +0100

    Fix puppet

--- vagrant/modules/app_billing/manifests/install.pp
+++ vagrant/modules/app_billing/manifests/install.pp
@@ -1,24 +1,4 @@
 class app_billing::install {
-    mysql_user { 'billing@localhost':
-        ensure        => present,
-        password_hash => mysql_password("billing"),
-        require => File["/root/.my.cnf"]
-    }
-
-    mysql_database { 'billing':
-        ensure => present,
-        require => File["/root/.my.cnf"]
-    }
-
-    mysql_grant { 'billing@localhost/cart.*':
-        ensure     => 'present',
-        options    => ['GRANT'],
-        privileges => ['ALL'],
-        table      => 'billing.*',
-        user       => 'billing@localhost',
-        require    => [Mysql_user['billing@localhost'], Mysql_database['billing']]
-    }    
-
     file { "/opt/billing":
         owner => root,
         group => root,
