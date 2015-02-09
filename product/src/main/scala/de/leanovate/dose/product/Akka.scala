87951febd5fc9a0ef8f1b06ce27ffc9d3b514505
Untoldwind <landru@untoldwind.net>
2015-02-09 20:14:29 +0100

    Product service to own submodule

--- product/src/main/scala/de/leanovate/dose/product/Akka.scala
+++
@@ -1,9 +0,0 @@
-package de.leanovate.dose.product
-
-import akka.actor.ActorSystem
-
-object Akka {
-  implicit val actorSystem = ActorSystem("actorsystem")
-
-  implicit val executor = Akka.actorSystem.dispatcher
-}
--- product/src/main/scala/de/leanovate/dose/product/model/Products.scala
+++
@@ -1,9 +0,0 @@
-package de.leanovate.dose.product.model
-
-import spray.json.DefaultJsonProtocol
-
-case class Products(activeProducts:Seq[ActiveProduct])
-
-object Products extends DefaultJsonProtocol {
-  implicit val productsFormat = jsonFormat1(Products.apply)
-}
